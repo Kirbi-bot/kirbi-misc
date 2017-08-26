6e2b53b2d3cac2d87bae19ebea4e93501c70cd77
Nate Richardson <hello@naterichardson.com>
2017-08-25 23:29:29 -0400

    streamlining
    standardized package.json, added .gitignore, make module work correctly

--- misc.js
+++ misc.js
@@ -1,45 +1,45 @@
-exports.commands = [
-	'lmgtfy',
-	'unshorten',
-	'ping',
-	'say'
-]
+const unshort = require('unshort');
 
-var unshort = require('unshort');
-
-exports.lmgtfy = {
-	usage: '<Let Me Google that for You>',
-	description: 'Plebs, plz.',
-	process: (msg, suffix, isEdit, cb) => {
-		if (suffix) {
-			cb(`<http://lmgtfy.com/?q=${encodeURI(require("remove-markdown")(suffix))}>`, msg, false, true);
-		}
-	}
-}
-
-exports.unshorten = {
-	usage: '<link to shorten>',
-	description: 'Unshorten a link.',
-	process: (msg, suffix, isEdit, cb) => {
-		if (suffix) {
-			unshort(suffix, function (err, url) {
-				if (url) {
-					cb(`Original url is: ${url}`, msg);
-				} else {
-					cb('This url can\'t be expanded', msg);
+module.exports = function (config, auth) {
+	return {
+		commands: [
+			'lmgtfy',
+			'unshorten',
+			'ping',
+			'say'
+		],
+		lmgtfy: {
+			usage: '<Let Me Google that for You>',
+			description: 'Plebs, plz.',
+			process: (msg, suffix, isEdit, cb) => {
+				if (suffix) {
+					cb(`<http://lmgtfy.com/?q=${encodeURI(require("remove-markdown")(suffix))}>`, msg, false, true);
 				}
-			});
+			}
+		},
+		unshorten: {
+			usage: '<link to shorten>',
+			description: 'Unshorten a link.',
+			process: (msg, suffix, isEdit, cb) => {
+				if (suffix) {
+					unshort(suffix, function (err, url) {
+						if (url) {
+							cb(`Original url is: ${url}`, msg);
+						} else {
+							cb('This url can\'t be expanded', msg);
+						}
+					});
+				}
+			}
+		},
+		ping: {
+			description: 'responds pong, useful for checking if bot is alive',
+			process: (msg, suffix, isEdit, cb) => { cb(`${msg.author} pong!`, msg); }
+		},
+		say: {
+			usage: '<message>',
+			description: 'bot says message',
+			process: (msg, suffix, isEdit, cb) => { cb(suffix, msg); }
 		}
-	}
-}
-
-exports.ping = {
-	description: 'responds pong, useful for checking if bot is alive',
-	process: (msg, suffix, isEdit, cb) => { cb(`${msg.author} pong!`, msg); }
-}
-
-exports.say = {
-	usage: '<message>',
-	description: 'bot says message',
-	process: (msg, suffix, isEdit, cb) => { cb(suffix, msg); }
-}
+	};
+};
